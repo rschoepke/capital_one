5eff2264feb8ce8be2beba3d065a189557eebc13
schoepke <schoepke@umich.edu>
2018-03-14 00:53:12 -0400

    Initial commit

---
+++ capital_one_r_graphs.R
@@ -0,0 +1,5 @@
+data = read.csv(file = "C:/Users/rache/Desktop/Capital\ One/sfpd_dispatch_data_subset.csv", sep = " ", colClasses=c("NULL", "NULL", "NULL", "NULL", "NULL", "NULL", NA, NA, NA, NA, NA, NA, NA, "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL"))
+
+data_example = data[1,1]
+
+print(data)
