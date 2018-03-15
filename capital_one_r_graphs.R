890e99a3f122e2dac6dd7d8ba6e15da1fc3ffa63
schoepke <schoepke@umich.edu>
2018-03-14 23:35:16 -0400

    Reading in data and splitting time columns

--- capital_one_r_graphs.R
+++ capital_one_r_graphs.R
@@ -1,5 +1,25 @@
-data = read.csv(file = "C:/Users/rache/Desktop/Capital\ One/sfpd_dispatch_data_subset.csv", sep = " ", colClasses=c("NULL", "NULL", "NULL", "NULL", "NULL", "NULL", NA, NA, NA, NA, NA, NA, NA, "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL", "NULL"))
+library(tidyr)
 
-data_example = data[1,1]
+# Reads data into two data frames, separating time data
+data = read.csv(file="C:/Users/rache/Desktop/Capital\ One/sfpd_dispatch_data_subset.csv", header=FALSE, sep=",",
+                colClasses=c(rep("NULL", 6), rep("character", 7), "NULL", "character", rep("NULL", 21)))
+all_data = read.csv(file="C:/Users/rache/Desktop/Capital\ One/sfpd_dispatch_data_subset.csv", header=FALSE, sep=",",
+                    colClasses=c(rep("character", 6), rep("NULL", 7), "character", "NULL", rep("character", 21)))
 
-print(data)
+# Names columns that will be split for time
+names = c("received_timestamp", "entry_timestamp", "dispatch_timestamp", "response_timestamp", "on_scence_timestamp",
+          "transport_timestamp", "hospital_timestamp", "available_timestamp")
+colnames(data) = names
+
+data = data[-1,]
+all_data = all_data[-1,]
+
+# Splits time cells to separate the time from date and time zone
+for(i in 1:length(names)) {
+  temp_data = separate(data, col = names[i], into=c("Date", "Time", "Time Zone"), sep=" ")
+  temp_vec = temp_data[i]
+  all_data = cbind(temp_vec, all_data)
+  all_data = cbind(temp_data[i + 1], all_data)
+  all_data = cbind(temp_data[i + 2], all_data)
+}
+print(all_data)
